67439d801a9039d8ca0cc161d9289baadd994734
Abv191 <abvabv191@gmail.com>
2024-02-16 19:51:00 +0200

    Add function

---
+++ main.py
@@ -0,0 +1,19 @@
+import re
+
+
+def valid_phone(phone_number):
+    # Перевірка для формату номера телефону: +380 і 12 цифр
+    pattern = r'^\+380\d{9}$'
+    if re.match(pattern, phone_number):
+        return True
+    else:
+        return "Номер телефону неправильний. Перевірте формат: +380XXXXXXXXX"
+
+
+def valid_email(email):
+    # Перевірка для формату адреси електронної пошти з закінченням .com
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[cC][oO][mM]$'
+    if re.match(pattern, email):
+        return True
+    else:
+        return "Адреса електронної пошти неправильна. Перевірте формат та закінчення."
