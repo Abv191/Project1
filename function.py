b2f4cf4afbfc7d368f12476ca911b7f2266e40a9
Abv191 <abvabv191@gmail.com>
2024-02-17 19:25:20 +0200

    Fix function

--- function.py
+++ function.py
@@ -2,6 +2,8 @@ import re
 
 
 def valid_phone(phone_number):
+    # Видалення зайвих пробілів
+    phone_number = phone_number.strip()
     # Перевірка для формату номера телефону: +380 і 12 цифр
     pattern = r'^\+380\d{9}$'
     if re.match(pattern, phone_number):
@@ -11,6 +13,8 @@ def valid_phone(phone_number):
 
 
 def valid_email(email):
+    # Видалення зайвих пробілів
+    email = email.strip()
     # Перевірка для формату адреси електронної пошти з закінченням .com
     pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[cC][oO][mM]$'
     if re.match(pattern, email):
